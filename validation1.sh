f205501ea63369faa5cfb67d15b068d368e82635
ChinmayKumarDas <chinmay.das@microsoft.com>
2019-08-22 17:54:27 +0530

    Update validation1.sh

--- validation1.sh
+++ validation1.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+echo "Please enter the ResourceGroup name"
+read rgname
+echo "please enter the cluster name"
+read clname
+az aks get-credentials -g $rgname -n $clname &>/dev/null
+
+nodecount=`kubectl get nodes | grep aks | wc -l` >/tmp/scen2
+if [ $nodecount == 5 ]
+then
+echo stopeffortsweet
+else
+echo "Scenario 2 result is FAILED"
+fi
+[azuser@ansiblemanage-0 aksscripts]$ cat validation1.sh
 !/bin/bash
 (kubectl get nodes | grep "NotReady") >/dev/null
 if [ $? -eq 0 ]
@@ -6,3 +21,4 @@ echo "Scenario 1 is FAILED"
 else
 echo "hometradebroke"
 fi
+
