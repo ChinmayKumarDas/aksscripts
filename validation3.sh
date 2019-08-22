da3176c41ca29b07e74451a923d4d3f05aa6f3b7
ChinmayKumarDas <chinmay.das@microsoft.com>
2019-08-22 18:16:24 +0530

    22/08/2019

--- validation3.sh
+++ validation3.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Please enter the ResourceGroup name"
+read rgname
+echo "please enter the cluster name"
+read clname
+az aks get-credentials -g $rgname -n $clname &>/dev/null
 nodecount=`kubectl get nodes | grep aks | wc -l` >/tmp/scen2
 if [ $nodecount == 2 ]
 then
--- validation5.sh
+++ validation5.sh
@@ -1,4 +1,9 @@
-!/bin/bash
+#!/bin/bash
+echo "Please enter the ResourceGroup name"
+read rgname
+echo "please enter the cluster name"
+read clname
+az aks get-credentials -g $rgname -n $clname &>/dev/null
 (kubectl get nodes | grep "NotReady") >/dev/null
 if [ $? -eq 0 ]
 then
