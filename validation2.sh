674ed68612a364134487dd62f2cddc3fb7eff8e9
ChinmayKumarDas <chinmay.das@microsoft.com>
2019-08-22 17:56:30 +0530

    Update validation2.sh

--- validation2.sh
+++ validation2.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "Please enter the ResourceGroup name"
+read rgname
+echo "please enter the cluster name"
+read clname
+az aks get-credentials -g $rgname -n $clname &>/dev/null
+
 nodecount=`kubectl get nodes | grep aks | wc -l` >/tmp/scen2
 if [ $nodecount == 5 ]
 then
